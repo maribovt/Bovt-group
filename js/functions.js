07315da29a11897691f6b1149d59cee142a37ad5
maribovt <maribovt@gmail.com>
2019-09-05 19:16:19 +0300

    comment

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $(".btn-show-profile-info").click(function() {
+        $(".user-interface__popup").toggleClass("show");
+    });
+
+});
+
+
+jQuery(function() {
+    initMobileNav();
+});
+
+// mobile menu init
+function initMobileNav() {
+    jQuery('body').mobileNav({
+        menuActiveClass: 'nav-active',
+        menuOpener: '.nav-opener'
+    });
+}
+
+
